eb08692cc5753c8584b5c6e8c85a4fc1aa1abf89
maup007 <159116868+maup007@users.noreply.github.com>
2024-03-20 17:30:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ setup(
   name='hello-world-package',
   version='0.1',
   packages=['hello-world'],
-  license='MIT',
   description='sample pythin package to print "Hello, world!"',
   author='maup007',
   author_email='maup@tdc.dk',
   url='https://github.com/maup007/publish-python-github-packages',
-  keywords=['python', 'hello', 'world'],
 )
