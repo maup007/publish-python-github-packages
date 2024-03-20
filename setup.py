34754f21e64ec258a7d71b86d70571bcf10cae89
maup007 <159116868+maup007@users.noreply.github.com>
2024-03-20 10:48:06 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name='hello-world'
+  version='0.1',
+  packages=['hello'],
+  description='sample pythin package to print "Hello, world!"',
+  author='maup007',
+  author+email='maup007@tdc.dk',
+  url='https://github.com/maup007/publish-python-github-packages',
+)
